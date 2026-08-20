ba909ded59cd9c5758c7b69853977b6dcf8b62a4
Paul Light <nwosupaul3@gmail.com>
2026-08-20 13:39:11 +0100

    fix(payments): register hardened revenue verifier

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { RevenueVerificationService } from './revenue-verification.service';
 import { PaymentsController } from './payments.controller';
 import { StripeProvider } from './stripe.provider';
 import { DatabaseModule } from '../../database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [PaymentsService, StripeProvider],
+  providers: [PaymentsService, RevenueVerificationService, StripeProvider],
   controllers: [PaymentsController],
-  exports: [PaymentsService],
+  exports: [PaymentsService, RevenueVerificationService],
 })
 export class PaymentsModule {}
